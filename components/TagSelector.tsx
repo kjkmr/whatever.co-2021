dfe27dba5351c45a20f1d111ac664bdb7860dfc6
Saqoosha <a@saqoo.sh>
2021-01-18 17:33:43 +0900

    TagSelector

--- components/TagSelector.tsx
+++ components/TagSelector.tsx
@@ -5,26 +5,22 @@ import { Grad } from './Grad'
 
 const TagItem = ({ name, slug, focused }: { name: string, slug: string, focused: boolean }) => (
   <li className={classNames('tag-item', { focused })}>
-    <Link href={`/work/category/${slug}`} passHref prefetch={false}>
+    <Link href={`/work/category/${slug}`} passHref>
       <a><Grad><span className="inner">{name}</span></Grad></a>
     </Link>
     <style jsx>{`
-      vwpx(px)
-        'calc((100vw - 80px) * %s)' % (px / (1366 - 80))
+      @import 'lib/vw.styl'
       .tag-item
         position relative
         z-index 0
         display inline-block
-        padding 0
+        padding 0 50px
         margin 0
-        margin-right -1px
-        margin-bottom vwpx(28)
+        margin-right 10px
+        margin-bottom 10px
         list-style-type none
-        border 1px solid transparent
-        border-left 1px solid black
-        border-right 1px solid black
-        width vwpx(240)
-        height vwpx(39)
+        border 1px solid #B4B4B4
+        height 42px
         text-align center
         vertical-align middle
       a
@@ -33,16 +29,21 @@ const TagItem = ({ name, slug, focused }: { name: string, slug: string, focused:
         align-items center
         width 100%
         height 100%
-        padding-top 1px
         border none
       .focused
-        border 1px solid red
+        border-color red
+        border-width 2px
+        padding 0 49px
+        height 40px
         z-index 100
+        a
+          color red
       .inner
         display inline-block
-        font-size vwpx(14)
         letter-spacing 0.02em
         overflow hidden
+        margin-top 4px
+        font-size 1.4rem
     `}</style>
   </li>
 )
@@ -52,13 +53,10 @@ export const TagSelector = ({ tags, active }: { tags?: Tag[], active: string }) 
     <TagItem name="All" slug='all' focused={active == "all"} />
     {tags?.map(tag => <TagItem key={tag.slug} name={tag.name} slug={tag.slug} focused={tag.slug == active} />)}
     <style jsx>{`
-      vwpx(px)
-        'calc((100vw - 80px) * %s)' % (px / (1366 - 80))
+      @import 'lib/vw.styl'
       .tag-selector
         padding 0
-        margin 0
-        margin-top 40px
-        margin-bottom vwpx(42)
+        margin 47px 80px vwpx(97)
     `}</style>
   </ol>
 )

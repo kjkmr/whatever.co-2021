a6ade138e9c53f3f77c2d4c9fc5a6441377fc20b
Saqoosha <a@saqoo.sh>
2021-01-26 11:55:20 +0900

    Remove unused

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { GetStaticProps } from 'next'
 import Link from 'next/link'
 import { Member, getAllMembers } from 'lib/api'

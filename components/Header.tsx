5b627b386ce75c6792040d14869a61c8c3ed0c4f
Saqoosha <a@saqoo.sh>
2021-01-29 18:35:20 +0900

    Header for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
 
 const Header = () => (
-  <div className="header">
-    <div className="fixed">
-      <Link href="/"><a><img src="/logo_rgb_black.png" /></a></Link>
+  <>
+    <div className="header">
+      <div className="fixed">
+        <Link href="/"><a><img src="/logo_rgb_black.png" /></a></Link>
+      </div>
     </div>
-
     <style jsx>{`
       .header
         height 80px
@@ -21,8 +22,15 @@ const Header = () => (
         padding 0
         margin-top 32px
         margin-right 30px
+      @media (--mobile)
+        .header
+          height 50px
+        a
+          margin-top 18.5px
+          img
+            width 75px
     `}</style>
-  </div>
+  </>
 )
 
 export default Header

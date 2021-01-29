8a63dba771ad30cbb69be630d760f67e5b25db36
Saqoosha <a@saqoo.sh>
2021-01-29 15:56:34 +0900

    LanguageSelector accepts className prop

--- components/LanguageSelector.tsx
+++ components/LanguageSelector.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import classnames from 'classnames'
 
 const LangLink = ({ lang, label, activeColor, inactiveColor, onSelected }: { lang: string, label: string, activeColor: string, inactiveColor: string, onSelected: any }) => {
   const { locale, pathname, query } = useRouter()
@@ -20,10 +21,17 @@ const LangLink = ({ lang, label, activeColor, inactiveColor, onSelected }: { lan
   )
 }
 
+export type LanguageSelectorType = {
+  activeColor?: string
+  inactiveColor?: string
+  separator?: string
+  onSelected?: any
+  className?: string
+}
 
-const LanguageSelector = ({ activeColor = 'black', inactiveColor = '#cccccc', separator = ' / ', onSelected = null }: { activeColor?: string, inactiveColor?: string, separator?: string, onSelected?: any }) => (
+const LanguageSelector = ({ activeColor = 'black', inactiveColor = '#cccccc', separator = ' / ', onSelected = null, className = '' }: LanguageSelectorType) => (
   <>
-    <div className="language-selector"><LangLink lang="ja" label="JA" activeColor={activeColor} inactiveColor={inactiveColor} onSelected={onSelected} /><span className="sp" style={{ color: inactiveColor }} >{separator}</span><LangLink lang="en" label="EN" activeColor={activeColor} inactiveColor={inactiveColor} onSelected={onSelected} /><span className="sp" style={{ color: inactiveColor }}>{separator}</span><LangLink lang="zh-hans" label="ZH" activeColor={activeColor} inactiveColor={inactiveColor} onSelected={onSelected} /></div>
+    <div className={classnames('language-selector', className)}><LangLink lang="ja" label="JA" activeColor={activeColor} inactiveColor={inactiveColor} onSelected={onSelected} /><span className="sp" style={{ color: inactiveColor }} >{separator}</span><LangLink lang="en" label="EN" activeColor={activeColor} inactiveColor={inactiveColor} onSelected={onSelected} /><span className="sp" style={{ color: inactiveColor }}>{separator}</span><LangLink lang="zh-hans" label="ZH" activeColor={activeColor} inactiveColor={inactiveColor} onSelected={onSelected} /></div>
     <style jsx>{`
       .language-selector
         font-size 1.2rem

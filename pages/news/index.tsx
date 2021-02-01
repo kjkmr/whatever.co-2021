ba7bddb9e2f6ac6ba81ce9fd9e3e69061c682b9e
Saqoosha <a@saqoo.sh>
2021-02-01 20:02:34 +0900

    News index for mobile

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -6,6 +6,8 @@ import { Entry, getNews } from 'lib/api'
 import Layout from 'components/Layout'
 import { Grad, GradImg } from 'components/Grad'
 
+const removeHtmlTags = (html: string) => html.replace(/<\/?[^>]+(>|$)/g, "")
+
 const NewsItem = ({ entry }: { entry: Entry }) => {
   const [entered, setEntered] = useState(false)
   return (
@@ -16,7 +18,7 @@ const NewsItem = ({ entry }: { entry: Entry }) => {
             <GradImg mouseEntered={entered}><img src={entry.hero_image} alt="" /></GradImg>
             <div><Grad className="date" inline>{entry.date}</Grad></div>
             <div><Grad className="title" inline><div dangerouslySetInnerHTML={{ __html: entry.title }} /></Grad></div>
-            <div><Grad className="desc" inline><div className="desc-inner" dangerouslySetInnerHTML={{ __html: entry.content?.split('<!--more-->')[0] || '' }}></div></Grad></div>
+            <div><Grad className="desc"><div className="desc-inner">{removeHtmlTags(entry.content?.split('<!--more-->')[0] || '')}</div></Grad></div>
           </a>
         </Link>
       </div>
@@ -35,8 +37,8 @@ const NewsItem = ({ entry }: { entry: Entry }) => {
             margin-top 3.0rem
           :global(.title)
             font-size 2.0rem
-            font-weight bold
-            line-height 3.0rem
+            font-weight 700
+            line-height 1.5
             margin-top 1.3rem
           :global(.desc)
             margin-top 1.6rem
@@ -49,12 +51,33 @@ const NewsItem = ({ entry }: { entry: Entry }) => {
             text-overflow ellipsis
             font-size var(--font-size-ja)
             font-weight 300
-            line-height 3.0rem
+            line-height 2.0
             :global(p)
               margin 0
             :global(a)
               border none
               padding 0
+        @media (--mobile)
+          @import 'lib/vw-mobile.styl'
+          .news-item
+            width 100%
+            margin 0 0 6.5rem 0
+            img
+              width vwpx(325)
+              height vwpx(325 / 16 * 9)
+            :global(.date)
+              font-size 1.0rem
+              margin-top 2.55rem
+            :global(.title)
+              font-size 1.7rem
+              line-height 1.55
+              margin 1.05rem 30px 0 0
+            :global(.desc)
+              width auto
+              margin 0.9rem 30px 0 0
+            .desc-inner
+              line-height 2.1
+              height 6.3em
       `}</style>
     </>
   )
@@ -77,6 +100,11 @@ const NewsIndex = ({ entries }: { entries: Entry[] }) => {
           display grid
           grid-template-columns 1fr 1fr
           grid-gap vwpx(92) vwpx(100)
+        @media (--mobile)
+          @import 'lib/vw-mobile.styl'
+          .news-index
+            margin 0
+            display block
       `}</style>
     </>
   )

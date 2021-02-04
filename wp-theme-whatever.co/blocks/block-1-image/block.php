1c9dcfc5d0a0495dbcc161d960b89568598f3b4e
Saqoosha <a@saqoo.sh>
2021-02-04 18:44:28 +0900

    WP theme

---
+++ wp-theme-whatever.co/blocks/block-1-image/block.php
@@ -0,0 +1,3 @@
+<div class="block-images block-1-image">
+  <img class="block-image-large" src="<?php block_field('image-1'); ?>" alt="">
+</div>
---
+++ wp-theme-whatever.co/blocks/block-4-images-b/block.php
@@ -0,0 +1,6 @@
+<div class="block-images block-4-images-b">
+  <img class="block-image-medium" src="<?php block_field('image-1');?>" alt="">
+  <img class="block-image-small" src="<?php block_field('image-2');?>" alt="">
+  <img class="block-image-small" src="<?php block_field('image-3');?>" alt="">
+  <img class="block-image-medium" src="<?php block_field('image-4');?>" alt="">
+</div>

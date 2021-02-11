4692c8eb7e0feb2b6945eea0d9bfe14b478addef
Saqoosha <a@saqoo.sh>
2021-02-11 15:57:06 +0900

    Update WP theme styles

--- wp-theme-whatever.co/functions.php
+++ wp-theme-whatever.co/functions.php
@@ -17,6 +17,12 @@ function whatever_theme_setup()
 }
 add_action('after_setup_theme', 'whatever_theme_setup');
 
+function my_admin_enqueue_scripts()
+{
+  wp_enqueue_style('my-google-font', '//fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;500;700&display=block', false, null, 'all');
+}
+add_action('admin_enqueue_scripts', 'my_admin_enqueue_scripts');
+
 add_image_size("hero_image_mobile", 1200, 1200, true);
 
 function disable_image_sizes($new_sizes)
@@ -30,3 +36,10 @@ function disable_image_sizes($new_sizes)
 }
 add_filter('intermediate_image_sizes_advanced', 'disable_image_sizes');
 add_filter('big_image_size_threshold', '__return_false');
+
+function extend_tiny_mce_before_init($mce_init)
+{
+  $mce_init['cache_suffix'] = 'v=' . time();
+  return $mce_init;
+}
+add_filter('tiny_mce_before_init', 'extend_tiny_mce_before_init');

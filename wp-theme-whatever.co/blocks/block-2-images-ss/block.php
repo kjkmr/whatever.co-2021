b1f409c8d3428506a081e04459ec4be04655c567
Saqoosha <a@saqoo.sh>
2021-02-12 15:06:42 +0900

    Fix image block ss

--- wp-theme-whatever.co/blocks/block-2-images-ss/block.php
+++ wp-theme-whatever.co/blocks/block-2-images-ss/block.php
@@ -1,4 +1,4 @@
 <div class="block-images block-2-images-ss">
-  <img class="block-image-large" src="<?php block_field('image-1'); ?>" alt="">
-  <img class="block-image-medium" src="<?php block_field('image-2'); ?>" alt="">
+  <img class="block-image-small" src="<?php block_field('image-1'); ?>" alt="">
+  <img class="block-image-small" src="<?php block_field('image-2'); ?>" alt="">
 </div>

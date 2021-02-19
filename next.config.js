424a7546d05ba791354a81f3b772704d98ff41f8
Saqoosha <a@saqoo.sh>
2021-02-19 19:21:29 +0900

    Add redirect setting

--- next.config.js
+++ next.config.js
@@ -1,16 +1,29 @@
+const redirects = require('./redirects.json')
+
 module.exports = {
   i18n: {
     locales: ['ja', 'en', 'zh-hant'],
     defaultLocale: 'en',
   },
+  trailingSlash: true,
   async redirects() {
     return [
       {
         source: '/work',
-        destination: '/work/category/all',
+        destination: '/work/category/all/',
+        permanent: false,
+      },
+      {
+        source: '/zh/:slug/',
+        destination: '/zh-hant/:slug/',
         permanent: true,
       },
-    ]
+      {
+        source: '/zh/team/:slug/',
+        destination: '/zh-hant/team/:slug/',
+        permanent: true,
+      },
+    ].concat(redirects)
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {

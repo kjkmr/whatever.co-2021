cf747bf02e22f50d8869a403b35e1906c576805b
Saqoosha <a@saqoo.sh>
2021-02-22 17:42:59 +0900

    Setup Google Analytics
    https://github.com/vercel/next.js/tree/canary/examples/with-google-analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,27 @@
+import { useEffect } from 'react'
 import App from 'next/app'
 import type { AppProps, AppContext } from 'next/app'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { loadResources } from 'lib/i18n'
 import { FaviconAnimator } from 'lib/FaviconAnimator'
 import { useLayoutEffect } from 'lib/useLayoutEffect'
+import * as gtag from 'lib/gtag'
 import 'styles/global.sass'
 
 let animator: FaviconAnimator
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   useLayoutEffect(() => {
     if (!animator) {
       animator = new FaviconAnimator()

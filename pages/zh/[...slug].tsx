19cd269954565c9672a90f4683ca84d9ab863510
Saqoosha <a@saqoo.sh>
2021-02-25 19:08:15 +0900

    permanent: true

--- pages/zh/[...slug].tsx
+++ pages/zh/[...slug].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   }
   return {
     redirect: {
-      permanent: false,
+      permanent: true,
       destination: destination
     }
   }

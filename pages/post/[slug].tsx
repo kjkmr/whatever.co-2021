6118267dca06022836c83b31d244b157957ada9d
Saqoosha <a@saqoo.sh>
2021-02-26 16:54:36 +0900

    /en to /

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,18 +4,26 @@ import { findRedirectDest } from 'lib/redirect'
 const Post = () => <></>
 export default Post
 
+const getLocalePath = (locale: string): string => {
+  switch (locale) {
+    case 'en': return ''
+    case 'zh': return '/zh-hant'
+  }
+  return `/${locale}`
+}
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { req, params, locale } = context
   const destPath = findRedirectDest(params?.slug as string)
   if (destPath) {
     // @ts-ignore
-    const newLocale = req['__nextStrippedLocale']
-      ? (locale == 'zh' ? 'zh-hant' : locale)
-      : 'ja'
+    const newLocalePath = req['__nextStrippedLocale']
+      ? getLocalePath(locale!)
+      : '/ja'
     return {
       redirect: {
         permanent: true,
-        destination: `/${newLocale}${destPath}`
+        destination: `${newLocalePath}${destPath}`
       }
     }
   }

792afa5a1755f4e28ca84d979365c801b10b7fa8
Saqoosha <a@saqoo.sh>
2021-02-26 15:58:56 +0900

    Add preview secret

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -5,8 +5,9 @@ const VALID_CATEGORIES = ['work', 'news', 'team']
 const VALID_LOCALES = ['ja', 'en', 'zh-hant']
 
 export default async function preview(req: NextApiRequest, res: NextApiResponse) {
-  const { slug, category, locale } = req.query
-  if (!slug || Array.isArray(slug)
+  const { secret, slug, category, locale } = req.query
+  if (!secret || Array.isArray(secret) || secret !== process.env.WP_PREVIEW_SECRET
+    || !slug || Array.isArray(slug)
     || !category || Array.isArray(category) || VALID_CATEGORIES.indexOf(category) == -1
     || !locale || Array.isArray(locale) || VALID_LOCALES.indexOf(locale) == -1) {
     return res.status(401).send('Invalid params')

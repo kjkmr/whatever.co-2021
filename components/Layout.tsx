1850b9dca8dd8b952f9f19144a08d5ebba3ae05f
Saqoosha <a@saqoo.sh>
2021-05-20 16:24:55 +0900

    RSS Autodiscovery

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -63,6 +63,7 @@ const Layout = ({ children, footer, title = '', side = '', backto = '', showHead
   }
   const router = useRouter()
   const locale = router.locale || router.defaultLocale!
+  const feedPath = (locale == 'en' ? '' : `/${locale}`) + '/feed/'
   return (
     <>
       <Head>
@@ -71,6 +72,7 @@ const Layout = ({ children, footer, title = '', side = '', backto = '', showHead
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/web-animations/2.3.2/web-animations.min.js" integrity="sha512-oAY57i8MXmaOP7pAylNLnULAM4QLV3uGnvnXVY4zF229/zFzTvG2/5YIgH8iN8oZR2hnbkiDPd4JCJGaH4oG6g==" crossOrigin="anonymous"></script>
+        <link rel="alternate" type="application/rss+xml" title="Whatever Inc. News" href={feedPath} />
       </Head>
       <div>
         <div className="container" key={locale} style={templateStyle}>
